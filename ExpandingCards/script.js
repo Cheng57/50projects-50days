3707b59bcc42f738945ec2fea38a919a2d4716d1
Cheng Wu <cwu57cn@gmail.com>
2024-10-13 13:21:20 -0500

    Update file

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,1 +1,12 @@
 "use strict";
+const boxes = document.querySelectorAll(".box");
+console.log(boxes);
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    boxes.forEach((box) => {
+      box.classList.remove("expand");
+    });
+    box.classList.add("expand");
+  });
+});
